c5522dd2b4819b748be47bd98b3fa178841e056c
Frozenmocca <61995971+Frozenmocca@users.noreply.github.com>
2020-03-28 15:27:49 -0500

    Add files via upload
    Trying to do this again

--- src/components/todos/AddTodo.js
+++ src/components/todos/AddTodo.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function AddTodo(props) {
+const AddTodo = ({addTodoItem}) => {
+
+  let playerInput = React.createRef();
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    addTodoItem(playerInput.current.value);
+    e.currentTarget.reset();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form className="w-100" onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          ref={playerInput}
+          placeholder="Enter a new task" 
+        />
+
+        <input 
+        type="submit"
+        value="Add task"
+        />
+      </form>
   )
 }
 
--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import TodoItem from './TodoItem';
 import './Todos.scss';
+import AddTodo from './AddTodo';
 
 class Todos extends Component {
   state = {
@@ -15,25 +16,53 @@ class Todos extends Component {
 
   getTodoItems = () => {
     fetch('https://jsonplaceholder.typicode.com/todos/')
-      .then(res => res.json())
-      .then(todos => {
-        const todoItems = !Array.isArray(todos) ? new Array(todos) : todos;
-        this.setState({ todos: todoItems })
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    .then(res => res.json())
+    .then(todos => {
+      const todoItems = !Array.isArray(todos) ? new Array(todos) : todos;
+      this.setState({ todos: todoItems })
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
+
 
   renderTodoItems = () => {
     let todos = [];
-
+    
     // Slicing to only show first 10 results for now
     this.state.todos.slice(0, 10).forEach((todo) => {
-      todos.push(<TodoItem key={todo.id} todo={todo} />);
+      todos.push(<TodoItem key={todo.id} todo={todo} deleteTodoItem={this.handleDeleteTodoItem}/>);
     });
-
     return todos;
+  }
+
+  handleDeleteTodoItem = (id) => {
+    this.setState( prevState => {
+        return {
+          todos: prevState.todos.filter(p => p.userId === 1 && p.id !== id)
+        };
+    });
+  }
+
+  handleAddTodoItem = (task) => {
+    let arrayLengthIndex = (this.state.todos.length);
+
+    this.setState( prevState => {
+      return {
+        todos: [
+          {
+            key: arrayLengthIndex+1,
+            userId: 1,
+            id: arrayLengthIndex+1,
+            title: task,
+            completed: false
+          },
+          ...prevState.todos
+        ]
+      }
+      
+    })
   }
 
   // render the component
@@ -42,6 +71,9 @@ class Todos extends Component {
       <div className="w-100">
         <div className="py-3 bg-primary text-white text-center">
           <h1 className="mb-0">TODOS</h1>
+        </div>
+        <div className = "my-3 py-2 bg-white text-center">
+          <AddTodo addTodoItem = {this.handleAddTodoItem}/>
         </div>
         <div className="container">
           <div className="row">
