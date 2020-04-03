0f676a71f3cbbfaec7ad70f4f56135da94a15382
Frozenmocca <61995971+Frozenmocca@users.noreply.github.com>
2020-04-02 22:08:29 -0500

    Added guards
    Added guards to the input field. Prevented inputs less than 3 in length or inputs containing "; , `, ~"

--- src/components/todos/AddTodo.js
+++ src/components/todos/AddTodo.js
@@ -1,41 +1,61 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-const AddTodo = ({addTodoItem}) => {
+class AddTodo  extends Component {
+  state = {
+    buttonType:"",
+    buttonState:"btn btn-primary w-100 disabled"
+  };
 
-  let userInput = React.createRef();
-  let handleSubmit = (e) => {
-    e.preventDefault();
-    addTodoItem(userInput.current.value);
-    e.currentTarget.reset();
+  render(){
+
+    const {addTodoItem}=this.props;
+  
+    let userInput = React.createRef();
+
+    let handleSubmit = (e) => {
+      e.preventDefault();
+      addTodoItem(userInput.current.value);
+      e.currentTarget.reset();
+      this.setState({buttonType:"null"});
+    }
+
+    let handleIsValid = () => {
+      let validUserInput = userInput.current.value;
+      let unvalidChar = new RegExp(/[;`~]/);
+      let test = unvalidChar.test(validUserInput);
+      
+      if(test || userInput.current.value.length < 3){
+        this.setState({buttonType:"null", buttonState:"btn btn-primary w-100 disabled"}) 
+      }
+      else if(userInput.current.value.length > 3){
+        this.setState({buttonType:"submit", buttonState:"btn btn-primary w-100"});
+      } 
+    }
+
+
+    return (
+      <form className="w-100 mt-3" onSubmit={handleSubmit} onChange={handleIsValid}>
+        <div className="row">
+          <div className="col-10">
+            <input 
+              type="text"
+              className="form-control"
+              ref={userInput}
+              placeholder="Enter a new task" 
+            />
+          </div>
+          <div className="col-2">
+            <input
+              className={this.state.buttonState}
+              type={this.state.buttonType}
+              value="Add task"
+            />
+          </div>
+        </div>
+      </form>
+    )
   }
-
-  return (
-    <form className="w-100 mt-3" onSubmit={handleSubmit}>
-      <div className="row">
-        <div className="col-10">
-          <input 
-            type="text"
-            className="form-control"
-            ref={userInput}
-            placeholder="Enter a new task" 
-          />
-        </div>
-        <div className="col-2">
-          <input
-            className="btn btn-primary w-100" 
-            type="submit"
-            value="Add task"
-          />
-        </div>
-      </div>
-    </form>
-  )
-}
-
-AddTodo.propTypes = {
-  userInput: PropTypes.string,
-  addTodoItem: PropTypes.func
 }
 
 export default AddTodo
