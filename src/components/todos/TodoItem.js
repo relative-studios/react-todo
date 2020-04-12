c91b05bdc586fc066fb2e470d0db5f1c0bab629d
relative-studios <38569266+relative-studios@users.noreply.github.com>
2020-04-11 20:05:45 -0500

    Added in API call to delete todo item from DB

--- src/components/todos/TodoItem.js
+++ src/components/todos/TodoItem.js
@@ -6,15 +6,15 @@ import './TodoItem.scss';
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
-
   state = {
     isClicked: false,
   }
 
   render(){
-
-    const { todo, deleteTodoItem, id } = this.props;
+    // User destructuring to grab todo item and deleteTodo method
+    const { todo, deleteTodoItem } = this.props;
     
+    // Update local state of isClicked
     const toggleClick = () => {
       this.setState({
         isClicked: !this.state.isClicked
@@ -23,16 +23,16 @@ class TodoItem extends Component {
 
     return (
       <div className="clearfix my-3 py-3 bg-white row">
-        <TodoCheckbox isClicked={this.state.isClicked} toggleClick={toggleClick} id={id}/>
+        <TodoCheckbox isClicked={this.state.isClicked} toggleClick={toggleClick} id={todo._id}/>
         <div className="float-right col-10 pt-2">
           <p className={`w-100 todo-item .text-truncate d-block ${this.state.isClicked ? 'strike':''}`}>
-            {todo.title}
+            {todo.todoItem.title}
             <FontAwesomeIcon 
               icon={faTrashAlt} 
               size="lg" 
               color="danger" 
               className="float-right text-danger pointer"
-              onClick={() => deleteTodoItem(todo.id)}
+              onClick={() => deleteTodoItem(todo._id)}
             />
           </p>
         </div>
