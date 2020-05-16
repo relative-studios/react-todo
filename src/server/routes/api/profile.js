2e8ee138cb9bcfb27bd52aa2432a82ce5d74aa41
Frozenmocca <61995971+Frozenmocca@users.noreply.github.com>
2020-05-15 19:14:33 -0500

    Added profile route

---
+++ src/server/routes/api/profile.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+let User = require('../../models/user.model.js');
+
+router.route("/").get((req, res) => {
+  //Grab the username from the query
+  const { username } = req.query;
+
+  //CHANGES FOR THE PROFILE COMPONENT
+  //Search the users and get the user object specified by Id
+  User.findOne({ username })
+    .then(user => {
+      const formattedUser = {
+        username: user.username,
+        name: user.name,
+        email: user.email,
+        duedate: user.duedate
+      };
+      
+      res.send(formattedUser);
+    })
+});
+
+module.exports = router;
