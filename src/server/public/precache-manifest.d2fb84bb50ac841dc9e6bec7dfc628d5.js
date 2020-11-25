691d09d0cd86c577bd0a3e49aa288403c68b773c
relative-studios <38569266+relative-studios@users.noreply.github.com>
2020-11-25 09:39:24 -0600

    updated build process for heroku

---
+++ src/server/public/precache-manifest.d2fb84bb50ac841dc9e6bec7dfc628d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ac65ae172fe98a70673209bb5e1f9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c33a9c46477019ceedf",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "a851c5fe61bbdd2252ad",
+    "url": "/static/css/main.d82de54a.chunk.css"
+  },
+  {
+    "revision": "7c33a9c46477019ceedf",
+    "url": "/static/js/2.e554690e.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/static/js/2.e554690e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a851c5fe61bbdd2252ad",
+    "url": "/static/js/main.ac5cdf40.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
